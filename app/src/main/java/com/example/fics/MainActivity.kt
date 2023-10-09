c90b484747086b65055ffaaae3dbaedd280ef4c6
tanveerm176 <tanveerm176@gmail.com>
2023-10-09 19:13:45 -0400

    added comments to explain code

--- app/src/main/java/com/example/fics/MainActivity.kt
+++ app/src/main/java/com/example/fics/MainActivity.kt
@@ -1,32 +1,47 @@
 package com.example.fics
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.fics.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
 
+
+class MainActivity : AppCompatActivity() {
+    //binding the mainActivity layout file to the mainActivity class
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        //allowing the app to dictate text color based on sys day/night mode
+        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_FOLLOW_SYSTEM)
+
+        //bind all layout files to the mainActivity class
+        //takes an XML file as input and builds the View objects from it
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+//        Log.v("binding root", binding.root.toString())
+        //sets the content of the activity to the first layout file in mobile_nav.xml
 
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_welcome, R.id.navigation_glossary, R.id.navigation_history, R.id.navigation_welcome
+                R.id.navigation_welcome, R.id.navigation_glossary, R.id.navigation_history, R.id.navigation_simulator
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
--- app/src/main/java/com/example/fics/ui/simulator/SimulatorFragment.kt
+++ app/src/main/java/com/example/fics/ui/simulator/SimulatorFragment.kt
@@ -8,29 +8,40 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.fics.databinding.FragmentSimulatorBinding
-import com.example.fics.ui.welcome.WelcomeViewModel
 
+
+//Each fragment in the UI is associated with the mainActivitu
+//think of them as subclasses that rely on a main class, although they define their own lifecycles
+//they are dependent on the activity the are linked to
+//fragments are reusable pieces of UI
 class SimulatorFragment : Fragment() {
 
     private var _binding: FragmentSimulatorBinding? = null
-
     // This property is only valid between onCreateView and
     // onDestroyView.
-    private val binding get() = _binding!!
 
+    private val binding get() = _binding!!
+    //the !! operator converts any value
+    // to a non-nullable type and throws an exception if the value is null
+
+    //the get() returns the non-null instance of _binding
+
+    //onCreateView() is one of the many fragment lifecycle funcs in Android
+    //creates and returns the view hierarchy associated with the fragment
+    //https://developer.android.com/reference/android/app/Fragment
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val welcomeViewModel =
-            ViewModelProvider(this).get(WelcomeViewModel::class.java)
+        val simulatorViewModel =
+            ViewModelProvider(this).get(SimulatorViewModel::class.java)
 
         _binding = FragmentSimulatorBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textSimulator
-        welcomeViewModel.text.observe(viewLifecycleOwner) {
+        simulatorViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
